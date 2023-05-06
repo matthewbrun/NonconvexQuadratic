1b61377dc44f546d5edadab41329148f29b7347a
Matthew Brun <matthewbrun@comcast.net>
2023-05-06 16:50:30 -0400

    add bash scripts

---
+++ submit_branching.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --exclusive
+
+# Loading the required module
+source /etc/profile
+module load julia
+
+# Declare output file
+out_file="branching_log.txt"
+# Create result file
+touch results/${out_file}
+# with reading, writing and executing permissions - why is executing needed?
+chmod +rwx results/${out_file}
+
+# run code while redirecting stderr to stdout with 2>&1 (https://stackoverflow.com/questions/818255/what-does-21-mean)
+julia branching_test.jl >&1 | tee results/${out_file}
